af330e6aebcb6334087f8ccca657c3cf09ea3ec8
Daniel Barde <mrbarde2k@gmail.com>
2019-08-21 01:22:29 +0100

    Switch to double quotes.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,61 +11,61 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: "User1",
     // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
+    // if it is not "/", like: "/test-site/img/image.jpg".
+    image: "/img/undraw_open_source.svg",
+    infoLink: "https://www.facebook.com",
     pinned: true,
   },
 ];
 
-const primary = '#F0DB4F';
-const secondary = '#323330';
+const primary = "#F0DB4F";
+const secondary = "#323330";
 
 const siteConfig = {
-  title: 'Learn Javascript', // Title for your website.
-  tagline: 'The ultimate guide to learning javascript.',
-  url: 'https://thecodecafe.github.io', // Your website URL
-  baseUrl: '/learn-js/', // Base URL for your project */
+  title: "Learn Javascript", // Title for your website.
+  tagline: "The ultimate guide to learning javascript.",
+  url: "https://thecodecafe.github.io", // Your website URL
+  baseUrl: "/learn-js/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  //   url: "https://facebook.github.io",
+  //   baseUrl: "/test-site/",
 
   // Used for publishing and more
-  projectName: 'learn-js',
-  organizationName: 'thecodecafe',
+  projectName: "learn-js",
+  organizationName: "thecodecafe",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+  //   organizationName: "JoelMarcey"
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'getting-started', label: 'Start Learning'},
-    {href: 'https://github.com/thecodecafe/learn-js/issues', label: 'Ask Questions', external: true},
+    {doc: "getting-started", label: "Start Learning"},
+    {href: "https://github.com/thecodecafe/learn-js/issues", label: "Ask Questions", external: true},
     {search: true},
-    {href: 'https://github.com/thecodecafe/learn-js', label: 'GtiHub', external: true},
+    {href: "https://github.com/thecodecafe/learn-js", label: "GtiHub", external: true},
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/dark-logo.png',
-  footerIcon: 'img/white-logo.png',
-  favicon: 'img/favicon.ico',
+  headerIcon: "img/dark-logo.png",
+  footerIcon: "img/white-logo.png",
+  favicon: "img/favicon.ico",
 
   /* Colors for website */
   colors: {
     brand: primary, // js yellow
-    dark: '#E0C420', // banana
-    deepdark: '#BAA020', // dark banana
-    light: '#FFEA6C', // light yellow
-    text: '#1a1a1a', // black substitute
-    subtle: '#6d6d6d', // light grey for text
-    divider: '#ececec', // very light grey
-    tintColor: '#f7f7f7', // slightly off white
-    backgroundColor: 'white',
+    dark: "#E0C420", // banana
+    deepdark: "#BAA020", // dark banana
+    light: "#FFEA6C", // light yellow
+    text: "#1a1a1a", // black substitute
+    subtle: "#6d6d6d", // light grey for text
+    divider: "#ececec", // very light grey
+    tintColor: "#f7f7f7", // slightly off white
+    backgroundColor: "white",
     primaryColor: primary, // js yellow
     secondaryColor: secondary, // js black
   },
@@ -87,20 +87,20 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default",
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: ["https://buttons.github.io/buttons.js"],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: "img/undraw_online.svg",
+  twitterImage: "img/undraw_tweetstorm.svg",
 
   disableHeaderTitle: true,
 
@@ -116,17 +116,17 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+  //   repoUrl: "https://github.com/facebook/test-site",
 
-  docsUrl: '',
+  docsUrl: "",
 
   algolia: {
-    appId: 'TPB7E4EB96',
-    indexName: 'learn-javascript',
-    apiKey: 'api-key-placeholder'
+    appId: "TPB7E4EB96",
+    indexName: "learn-javascript",
+    apiKey: "api-key-placeholder"
   },
 
-  repoUrl: 'https://github.com/thecodecafe/learn-js',
+  repoUrl: "https://github.com/thecodecafe/learn-js",
 
   scrollToTop: true,
   scrollToTopOptions: {
@@ -135,17 +135,17 @@ const siteConfig = {
     scrollDuration: 200
   },
   twitter: true,
-  twitterUsername: 'mrbarde2k',
-  usePrism: ['javascript', 'js', 'json'],
+  twitterUsername: "mrbarde2k",
   highlight: {
-    theme: 'tomorrow-night',
+    theme: "tomorrow-night",
   },
-  gaTrackingId: 'UA-145866759-1',
+  gaTrackingId: "UA-145866759-1",
   stylesheets: [
-    'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
-    'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900|Ubuntu&display=swap'
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+    "https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,900|Ubuntu&display=swap",
+    "https://afeld.github.io/emoji-css/emoji.css"
   ],
-  pageCodeStyle: require('./codeStyle'),
+  pageCodeStyle: require("./codeStyle"),
 };
 
 module.exports = siteConfig;
